e93b9077efc611d0662c146d2f5dffe394e68528
linhnnmt <162856460+linhnnmt@users.noreply.github.com>
2025-01-24 14:30:17 +0700

    Update CusomerApplication.java

--- consumer/src/main/java/com/spring/consumer/CusomerApplication.java
+++ consumer/src/main/java/com/spring/consumer/CusomerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CusomerApplication {
+public class ConsumerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CusomerApplication.class, args);
+        SpringApplication.run(ConsumerApplication.class, args);
     }
 }
